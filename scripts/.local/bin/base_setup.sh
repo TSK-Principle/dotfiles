b9f0c8d3f423a5bed7b47b27351a72932a516bbf
TSK-Principle <138122950+TSK-Principle@users.noreply.github.com>
2026-02-12 07:21:05 -0500

    update commit

--- scripts/.local/bin/base_setup.sh
+++ scripts/.local/bin/base_setup.sh
@@ -5,7 +5,7 @@ sudo sed -i 's/ main / main contrib /g' /etc/apt/sources.list
 sudo apt -y update && sudo apt -y upgrade
 
 # basic-debian
-sudo apt -y install git curl wget unzip network-manager htop picom fzf tmux keychain pass pass-otp oathtool stow gnupg ripgrep rclone mpv yt-dlp 7zip starship eza arp-scan sshfs jq pandoc ffmpeg openjdk-21-jdk maven htop nodejs npm bat gh translate-shell python3-pip
+sudo apt -y install git curl wget unzip network-manager htop picom fzf tmux keychain pass pass-otp oathtool stow gnupg ripgrep rclone mpv yt-dlp 7zip starship eza arp-scan sshfs jq pandoc ffmpeg openjdk-21-jdk maven btop nodejs npm bat gh translate-shell python3-pip
 
 # pip package
 pip install --user openai trafilatura --break-system-packages
